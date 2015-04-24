de3279c9ec14a7500ed332299b78d5ffbbb08884
rjmsilf <rjmsilfhout@gmail.com>
2015-04-24 11:01:09 +0000

    what is your name

--- Opdracht1/ManyGreetings.py
+++ Opdracht1/ManyGreetings.py
@@ -1,8 +1,2 @@
-<<<<<<< HEAD
 name=input('What is your name? ')
 print('Hello, %s!'%(name))
-=======
-name=input('Name? ')
-number = input('How many greetings? ')
-print(number  + ' x Hello, %s!'%(name))
->>>>>>> extension
