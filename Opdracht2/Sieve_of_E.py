710f00e970cc11d7829acc4ad78b58b482eb03f1
rjmsilf <rjmsilfhout@gmail.com>
2015-05-02 14:14:32 +0000

    verbeterde versie

--- Opdracht2/Sieve_of_E.py
+++ Opdracht2/Sieve_of_E.py
@@ -4,11 +4,12 @@ import sys
 T1 = time.perf_counter()
 
 def lijst(n):
-    lijst1=list(range(2,n+1))
-    for i in lijst1:
-        for j in lijst1:
-            if j%i == 0 and i != j:
-                lijst1.remove(j)
+    lijst1=list(range(0,n))
+    lijst1[1]=0
+    for i in range(1,n):
+        if lijst1[i] != 0:
+            for j in range(2*i,n,i):
+                lijst1[j]=0
     return lijst1
 
 T2 = time.perf_counter()
@@ -16,10 +17,13 @@ T2 = time.perf_counter()
 n=int(sys.argv[1])
 
 out=open(sys.argv[2], 'w')
+a=[]
 for i in lijst(n):
-    out.write(str(i)+'\n')
+    if i != 0:
+        out.write(str(i)+'\n')
+        a.append(i)
 out.close()
 
-aantal=len(lijst(n))
+aantal=len(a)
 
 print('Found '+str(aantal)+' prime numbers smaller than '+str(n)+' in '+str(T2-T1)+' sec.')
