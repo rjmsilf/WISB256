4f95b46eed1caf3e2662c202e214c84b647d25b0
rjmsilf <rjmsilfhout@gmail.com>
2015-05-09 13:48:07 +0000

    opdracht 3 inclusief bonus af

--- Opdracht3/estimate_pi.py
+++ Opdracht3/estimate_pi.py
@@ -2,6 +2,11 @@ import random
 import math
 import sys
 
+if len(sys.argv)==4:
+    seed=int(sys.argv[3])
+    random.seed(seed)
+    
+
 def naald(L):
     x=random.random()
     a=random.vonmisesvariate(0,0)
@@ -11,19 +16,31 @@ def naald(L):
     else:
         return True
 
+
 def dropnaald(N,L):
-    if L>1 or L<0:
-        print('L should be positive, and smaller than or equal to 1')
-        return
-    a = 0
-    for i in range(1,N+1):
-        if naald(L) == True:
-            a=a+1
-    pi=(2*L*N)/a
-    print(str(a)+' hits in '+str(N)+' tries \nPi = '+str(pi))
+    if L<=0:
+        return 'AssertionError: L should be positive'
+    if L<=1 and L>0:
+        a=0
+        for i in range(1,N+1):
+            if naald(L) == True:
+                a=a+1
+        pi=(2*L*N)/a
+        return str(a)+' hits in '+str(N)+' tries \nPi = '+str(pi)
+    elif L>1:
+        b=0
+        for j in range(1,N+1):
+            if naald(L) == True:
+                b=b+1
+        pi2=(2*N*(L-(L**2-1)**0.5 - math.asin(1/L)))/(b-N)
+        return str(b)+' hits in '+str(N)+' tries \nPi = '+str(pi2)
 
+def checkinput():
+    if len(sys.argv)<3:
+        return 'Use: estimate_pi.py N L'
+    else:
+        N=int(sys.argv[1])
+        L=float(sys.argv[2])
+        return dropnaald(N,L)
 
-L=int(sys.argv[2])
-N=int(sys.argv[1])
-
-print(dropnaald(N,L))
+print(checkinput())
