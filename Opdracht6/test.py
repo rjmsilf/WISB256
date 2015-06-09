1578abf3a32422e37b6bf2b5d1f328946b8e62a2
rjmsilf <rjmsilfhout@gmail.com>
2015-06-09 08:53:39 +0000

    a

--- Opdracht6/test.py
+++ Opdracht6/test.py
@@ -1,5 +1,7 @@
 from lorenz import *
 L1=Lorenz([-1,1,0])
 u1=L1.solve(50,.01)
-print(u1[0,0])
-print(u1[-1,0])
+L2=Lorenz([-1.001,1.001,.001])
+u2=L2.solve(50,.01)
+print(u1[0,0],u2[0,0])
+print(u1[-1,0],u2[-1,0])
